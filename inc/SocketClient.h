4245411a2a0ec489f306d7136e716c9bbaeb39c0
Matthew Landers <mtl2011@gmail.com>
2022-03-19 22:59:29 -0500

    Abstract class needs no impl -- removing

--- inc/SocketClient.h
+++ inc/SocketClient.h
@@ -1,9 +1,26 @@
 #ifndef SOCKETCLIENT_H_
 #define SOCKETCLIENT_H_
 
+#include <netinet/in.h>
+
 class SocketClient
 {
+public:
+    SocketClient();
+    virtual ~SocketClient();
 
+    virtual void createSocket() = 0;
+    virtual void bindSocket() = 0;
+    virtual void recvFrom() = 0;
+    virtual void sendTo() = 0;
+
+    int getFileDescriptor(){ return fd_; };
+    sockaddr_in getServInfo(){ return servaddr_; };
+
+protected:
+    int fd_; // file descriptor
+    struct sockaddr_in servaddr_; 
+    const unsigned short maxline_ = 1024;
 };
 
 #endif
